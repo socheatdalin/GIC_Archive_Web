b1ad2cce157204dfac3815032ad82296b5f32ff1
Socheatdalin <dal88246@gmail.com>
2023-10-30 17:18:36 +0700

    auth

--- src/components/Header/UserProfile.jsx
+++ src/components/Header/UserProfile.jsx
@@ -36,7 +36,7 @@ function UserProfile() {
                 setLastName(result.data.last_name);
                 setGeneration(result.data.generation);
                 setRole(result.data.role_name);
-                // setuserId(result.data.id);
+                setuserId(result.data.teacher_id);
             })
             .catch(err => {
                 console.log("Server error:", err);
